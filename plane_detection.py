afc7fcf3052af229c0637dfa73677bb55074b547
tahsin314 <tahsinmostafiz314@gmail.com>
2023-04-21 06:03:23 -0400

    plane detection added. Needs tuning

---
+++ plane_detection.py
@@ -0,0 +1,136 @@
+import cv2
+import numpy as np
+import open3d as o3d
+from open3d import geometry
+import matplotlib.pyplot as plt
+
+from rosbags.rosbag2 import Reader
+from rosbags.serde import deserialize_cdr
+
+#create paths and load data
+rgb_folder = './bagfiles/rgb_folder/'
+rgb_file = 'rosbag2_2023_04_14-17_38_27'
+# 'rosbag2_2023_04_12-17_57_22'
+# 'rosbag2_2023_04_14-16_28_36'
+# 'rosbag2_2023_04_14-16_41_33'
+# 'rosbag2_2023_04_14-16_40_18'
+
+# create reader instance and open for reading
+with Reader(rgb_folder+rgb_file) as reader:
+    # topic and msgtype information is available on .connections list
+    for connection in reader.connections:
+        print(connection.topic, connection.msgtype)
+
+    # iterate over messages
+    for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == '/color/preview/image':
+            img_msg = deserialize_cdr(rawdata, connection.msgtype)
+#             print(img_msg.header.frame_id)
+
+rgb = img_msg.data.reshape(250,250,3)
+rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+rgb = cv2.resize(rgb, (640,480), interpolation = cv2.INTER_AREA)
+rgb_img = o3d.geometry.Image(rgb)
+gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+gray_img = o3d.geometry.Image(gray)
+
+depth_folder = './bagfiles/depth_folder/'
+depth_file = 'rosbag2_2023_04_14-17_38_37'
+# 'rosbag2_2023_04_12-18_02_49'
+# 'rosbag2_2023_04_14-16_28_49'
+# 'rosbag2_2023_04_14-16_41_48'
+# 'rosbag2_2023_04_14-16_40_29'
+
+# create reader instance and open for reading
+with Reader(depth_folder+depth_file) as reader:
+    # topic and msgtype information is available on .connections list
+    for connection in reader.connections:
+        print(connection.topic, connection.msgtype)
+
+    # iterate over messages
+    for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == '/stereo/depth':
+            depth_msg = deserialize_cdr(rawdata, connection.msgtype)
+#             print(depth_msg.header.frame_id)
+
+f_depth = np.frombuffer(depth_msg.data, dtype=np.uint16)
+f_depth = f_depth.reshape(480,640)
+depth_img = o3d.geometry.Image(f_depth)
+rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb_img, depth_img, convert_rgb_to_intensity=True)
+print(rgbd)
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+    rgbd,
+    o3d.camera.PinholeCameraIntrinsic(
+        o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
+
+print(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+[a, b, c, d] = plane_model
+x = np.linspace(-1,1,10)
+y = np.linspace(-1,1,10)
+
+X,Y = np.meshgrid(x,y)
+Z = (d - a*X - b*Y) / c
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+surf = ax.plot_surface(X, Y, Z)
+# Flip it, otherwise the pointcloud will be upside down
+pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+inlier_cloud = pcd.select_by_index(inliers)
+outlier_cloud = pcd.select_by_index(inliers, invert=True)
+inlier_cloud.paint_uniform_color([1, 0, 0])
+outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+# o3d.visualization.draw_geometries([pcd])
+xyz = np.asarray(pcd.points)
+colors = None
+if pcd.has_colors():
+    colors = np.asarray(pcd.colors)
+elif pcd.has_normals():
+    colors = (0.5, 0.5, 0.5) + np.asarray(pcd.normals) * 0.5
+else:
+    geometry.paint_uniform_color((1.0, 0.0, 0.0))
+    colors = np.asarray(geometry.colors)
+print(colors.shape)
+
+# Single View
+# plt.figure(figsize=(15, 15))
+# ax = plt.axes(projection='3d')
+# # ax.view_init(90, -90)
+# ax.axis("off")
+# ax.scatter(xyz[:,0], xyz[:,1], xyz[:,2], s=0.05, c=colors)
+# plt.show()
+
+segment_models={}
+segments={}
+max_plane_idx=4
+rest=pcd
+for i in range(max_plane_idx):
+    colors = plt.get_cmap("tab20")(i)
+    segment_models[i], inliers = rest.segment_plane(distance_threshold=0.05,ransac_n=3,num_iterations=1000)
+    segments[i]=rest.select_by_index(inliers)
+    segments[i].paint_uniform_color(list(colors[:3]))
+    rest = rest.select_by_index(inliers, invert=True)
+    print("pass",i,"/",max_plane_idx,"done.")
+    [a, b, c, d] = segment_models[i]  
+    print(a,b,c,d)
+    x = np.linspace(-1,50,100)
+    y = np.linspace(-1,50,100)
+
+    X,Y = np.meshgrid(x,y)
+    Z = (d - a*X - b*Y) / c
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+
+    # surf = ax.plot_surface(X, Y, Z)
+    ax = plt.axes(projection='3d')
+# # ax.view_init(90, -90)
+# ax.axis("off")
+    ax.scatter(X, Y, Z, s=0.05, c=colors) 
+plt.show()
+# print(segment_models)
